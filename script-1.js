d4672f1407597ebda0e10f4e1f9d7cbd8dada907
mohammedayman6193-cpu <mohammedayman6193@gmail.com>
2025-09-09 13:56:28 +0530

    Add files via upload

---
+++ script-1.js
@@ -0,0 +1,11 @@
+// Interactive Email
+const email = document.getElementById("email");
+email.addEventListener("click", function() {
+    window.location.href = "mailto:ajayjeyakumar0307@gmail.com";
+});
+
+// Interactive Phone
+const phone = document.getElementById("phone");
+phone.addEventListener("click", function() {
+    window.location.href = "tel:+919789644237";
+});
